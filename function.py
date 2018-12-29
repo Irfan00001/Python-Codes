2f22fa6769745851a3666d0cf1a1072ac5177803
Irfan00001 <mugaleirfan@gmail.com>
2018-12-29 21:52:40 +0530

    Assignment #2

--- function.py
+++ function.py
@@ -1,7 +1,26 @@
-def addone(Number):
-    Output=Number+1
-    return Output
+#Function for Songs
+#Below are the functions created for each details as shown below
+def Artist():
+    name="Michael Jackson"
+    return name
+def Album():
+    name="They Don't Care About Us"
+    return name
+def Writers():
+    name="Michael Jackson"
+    return name
+def Duration():
+    name=4.9
+    return name
 
-var=1
-result=addone(var)
-print(result)
+#Calling each function and storing it in a Variable for further use
+artistname=Artist()
+albumname=Album()
+writername=Writers()
+duration=Duration()
+
+#Printing the data wich we get from the functionations
+print(artistname)
+print(albumname)
+print(writername)
+print(duration)
