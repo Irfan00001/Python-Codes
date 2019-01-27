f30f07a688e4d39dae5d8c06df5112f4b4d62cc0
Irfan00001 <mugaleirfan@gmail.com>
2019-01-27 15:34:18 +0530

    Assignment 11

---
+++ md5.py
@@ -0,0 +1,13 @@
+import hashlib
+import base64
+"""Assignment 11 Created"""
+
+def hash(workingkey):
+    key=bytes(workingkey,'utf-8')
+    result = hashlib.md5(key).hexdigest()
+    print("The Hash Value is : ", result)
+    print("To Check and verify the result, please go to the following url --> http://www.md5.cz/ online md5 genertor Tool")
+
+
+Input=input("Enter Key to Hash : ")
+hash(Input)
